1a612ca7964f181b1c8479c95914e52c38903f2f
Pie <slugslikepie@gmail.com>
2025-05-13 19:21:29 -0400

    Added dash cooldown

--- test_scenes/player_2d.gd
+++ test_scenes/player_2d.gd
@@ -10,14 +10,16 @@ const DEADBAND = 0.5
 const GROUND_MAX_SPEED := 400
 const GROUND_ACCELERATION := 6000
 const GROUND_DECELERATION := 9000
-const GROUND_ZERO_THRESHOLD := 120
+const GROUND_ZERO_VELOCITY_THRESHOLD := 120
 const AIR_MAX_SPEED := GROUND_MAX_SPEED
 const AIR_ACCELERATION := GROUND_ACCELERATION * 0.75
 const AIR_DECELERATION := GROUND_DECELERATION * 0.75
-const AIR_ZERO_THRESHOLD := GROUND_ZERO_THRESHOLD
+const AIR_ZERO_VELOCITY_THRESHOLD := GROUND_ZERO_VELOCITY_THRESHOLD
 
 # ud movement consts
 const CLIMB_MAX_SPEED := 200
+const MAX_GROUND_JUMPS := 1
+const MAX_AIR_JUMPS := 1
 const CLIMB_ACCELERATION := 4000
 const CLIMB_DECELERATION := 10000
 const CLIMB_ZERO_THRESHOLD := 150
@@ -32,12 +34,15 @@ const MAX_DASHES := 1
 const DASH_START_SPEED := 3000 / DASH_VELOCITY_SCALE
 const DASH_END_SPEED := 100.0
 const DASH_MAX_DURATION := 0.14 * DASH_VELOCITY_SCALE
+const DASH_COOLDOWN_DURATION := 0.2
 
 # Jump consts
-const GROUND_JUMP_SPEED := 500
-const JUMP_CANCEL_SPEED := 200 # UNUSED CURRENTLY
+const GROUND_JUMP_SPEED := 300
+const AIR_JUMP_SPEED := 250
 const WALL_CLIMBING_JUMP_VELOCITY := Vector2(200, 500)
 const WALL_SLIDING_JUMP_VELOCITY := Vector2(200, 100)
+const JUMP_CANCEL_SPEED := 200 # UNUSED CURRENTLY PROBABLY DELETE
+const JUMP_MAX_DURATION := 0.1
 
 
 # Grab consts
@@ -48,25 +53,32 @@ var gravity = Vector2(0, 0)
 
 # Movement vars
 var is_facing_right = true
-var lr_input := Input.get_axis("left", "right")
-var ud_input := Input.get_axis("up", "down")
+var lr_input_axis := Input.get_axis("left", "right")
+var ud_input_axis := Input.get_axis("up", "down")
 
 # Dash vars
-var dash_input := Input.is_action_just_pressed("dash")
+var is_dash_input_just_pressed := Input.is_action_just_pressed("dash")
 var dashes := 1 # Change MAX_DASHES to increase number of dashes
 var is_dashing := false
 var dash_timer := 0.0
+var dash_cooldown_timer := 0.0
 var dash_input_vector := Vector2.ZERO
 var dash_start_position := Vector2.ZERO
 var dash_end_position := Vector2.ZERO
 
 # Jump vars
-var jump_input := Input.is_action_just_pressed("jump")
-var is_jumping := false # NOT USED YET
+var is_jump_input_pressed := Input.is_action_pressed("jump")
+var is_jump_just_released = Input.is_action_just_released("jump")
+var ground_jumps := MAX_GROUND_JUMPS
+var air_jumps := MAX_AIR_JUMPS
+var is_jumping := false
+var jump_timer := 0.0
+var jump_cooldown_timer := 0.0
+var jump_start_location := "ground"
 
 # Grab vars
 var is_grab_inverted := true
-var grab_input := (Input.is_action_pressed("grab") or is_grab_inverted) and not (Input.is_action_pressed("grab") and is_grab_inverted)
+var is_grab_input_pressed := (Input.is_action_pressed("grab") or is_grab_inverted) and not (Input.is_action_pressed("grab") and is_grab_inverted)
 var is_on_left_wall := false
 var is_on_right_wall := false
 var is_climbing := false
@@ -79,25 +91,26 @@ func _physics_process(delta: float) -> void:
 	
 	gravity = get_gravity() * 1.3
 	
-	lr_input = Input.get_axis("left", "right")
-	ud_input = Input.get_axis("up", "down")
+	lr_input_axis = Input.get_axis("left", "right")
+	ud_input_axis = Input.get_axis("up", "down")
 
-	dash_input = Input.is_action_just_pressed("dash")
+	is_dash_input_just_pressed = Input.is_action_just_pressed("dash")
 
-	jump_input = Input.is_action_just_pressed("jump")
+	is_jump_input_pressed = Input.is_action_pressed("jump")
 	
-	grab_input = (Input.is_action_pressed("grab") or is_grab_inverted) and not (Input.is_action_pressed("grab") and is_grab_inverted)
-	is_climbing = grab_input and (is_on_left_wall or is_on_right_wall)
-	is_sliding = not grab_input and (is_on_left_wall or is_on_right_wall)
+	is_grab_input_pressed = (Input.is_action_pressed("grab") or is_grab_inverted) and not (Input.is_action_pressed("grab") and is_grab_inverted)
+	is_climbing = is_grab_input_pressed and (is_on_left_wall or is_on_right_wall)
+	is_sliding = not is_grab_input_pressed and (is_on_left_wall or is_on_right_wall)
 	
 	# Add dashing
 	# Starts dash
-	if dash_input and not is_dashing and dashes > 0:
+	if is_dash_input_just_pressed and not is_dashing and dashes > 0:
 		dash_input_vector = Input.get_vector("left", "right", "up", "down")
 		dash_timer = 0.0
 		velocity = Vector2.ZERO
 		is_dashing = true
 		dashes -= 1
+
 	# Moves player through dash
 	if is_dashing:
 		dash_timer += delta
@@ -112,13 +125,21 @@ func _physics_process(delta: float) -> void:
 		# Ends dash
 		if percent_complete >= 1:
 			dash_timer = 0
+			dash_cooldown_timer = 0
 			is_dashing = false
-	# Disables this block of code if the player is dashing to prevent other movements
+
+	if is_on_floor() and clamp(dash_cooldown_timer / DASH_COOLDOWN_DURATION, 0, 1) >= 1 and not is_dashing:
+		dashes = MAX_DASHES
+	elif not is_dashing:
+		dash_cooldown_timer += delta
+	print(dash_cooldown_timer)
+		
+	# Disables this block of code if the player is dashing or jumping to prevent other movements
 	if not is_dashing:
 		# Flips the direction the player is facing based off of user input
-		if lr_input > DEADBAND:
+		if lr_input_axis > DEADBAND:
 			is_facing_right = true
-		elif lr_input < -DEADBAND:
+		elif lr_input_axis < -DEADBAND:
 			is_facing_right = false
 			
 		# Add gravity
@@ -128,22 +149,21 @@ func _physics_process(delta: float) -> void:
 				velocity += gravity * delta
 			# Sliding gravity
 			elif is_sliding:
-				print((SLIDE_PASSIVE_ACCELERATION + SLIDE_ACTIVE_ACCELERATION_OFFSET * ud_input) * delta)
-				if velocity.y + (SLIDE_PASSIVE_ACCELERATION + SLIDE_ACTIVE_ACCELERATION_OFFSET * ud_input) * delta <= SLIDE_PASSIVE_MAX_SPEED + ud_input * SLIDE_ACTIVE_SPEED_OFFSET:
-					velocity.y += (SLIDE_PASSIVE_ACCELERATION + SLIDE_ACTIVE_ACCELERATION_OFFSET * ud_input) * delta
+				if velocity.y + (SLIDE_PASSIVE_ACCELERATION + SLIDE_ACTIVE_ACCELERATION_OFFSET * ud_input_axis) * delta <= SLIDE_PASSIVE_MAX_SPEED + ud_input_axis * SLIDE_ACTIVE_SPEED_OFFSET:
+					velocity.y += (SLIDE_PASSIVE_ACCELERATION + SLIDE_ACTIVE_ACCELERATION_OFFSET * ud_input_axis) * delta
 				else:
-					velocity.y = SLIDE_PASSIVE_MAX_SPEED + ud_input * SLIDE_ACTIVE_SPEED_OFFSET
+					velocity.y = SLIDE_PASSIVE_MAX_SPEED + ud_input_axis * SLIDE_ACTIVE_SPEED_OFFSET
 
 		# Add wall climbing
-		if grab_input and (is_on_left_wall or is_on_right_wall): # IS CLIMBING GOES HERE
-			if velocity.y <= CLIMB_MAX_SPEED and ud_input > DEADBAND:
-				if velocity.y + ud_input * CLIMB_ACCELERATION * delta <= CLIMB_MAX_SPEED:
-					velocity.y += ud_input * CLIMB_ACCELERATION * delta
+		if is_grab_input_pressed and (is_on_left_wall or is_on_right_wall): # IS CLIMBING GOES HERE
+			if velocity.y <= CLIMB_MAX_SPEED and ud_input_axis > DEADBAND:
+				if velocity.y + ud_input_axis * CLIMB_ACCELERATION * delta <= CLIMB_MAX_SPEED:
+					velocity.y += ud_input_axis * CLIMB_ACCELERATION * delta
 				else:
 					velocity.y = CLIMB_MAX_SPEED
-			elif velocity.y >= -CLIMB_MAX_SPEED and ud_input < -DEADBAND:
-				if velocity.y + ud_input * CLIMB_ACCELERATION * delta >= -CLIMB_MAX_SPEED:
-					velocity.y += ud_input * CLIMB_ACCELERATION * delta
+			elif velocity.y >= -CLIMB_MAX_SPEED and ud_input_axis < -DEADBAND:
+				if velocity.y + ud_input_axis * CLIMB_ACCELERATION * delta >= -CLIMB_MAX_SPEED:
+					velocity.y += ud_input_axis * CLIMB_ACCELERATION * delta
 				else:
 					velocity.y = -CLIMB_MAX_SPEED
 			else:
@@ -156,58 +176,89 @@ func _physics_process(delta: float) -> void:
 
 		# Apply left & right inputs:
 		if is_on_floor():
-			dashes = MAX_DASHES
-			if velocity.x <= GROUND_MAX_SPEED and lr_input > DEADBAND:
-				if velocity.x + lr_input * GROUND_ACCELERATION * delta <= GROUND_MAX_SPEED:
-					velocity.x += lr_input * GROUND_ACCELERATION * delta
+			if velocity.x <= GROUND_MAX_SPEED and lr_input_axis > DEADBAND:
+				if velocity.x + lr_input_axis * GROUND_ACCELERATION * delta <= GROUND_MAX_SPEED:
+					velocity.x += lr_input_axis * GROUND_ACCELERATION * delta
 				else:
 					velocity.x = GROUND_MAX_SPEED
-			elif velocity.x >= -GROUND_MAX_SPEED and lr_input < -DEADBAND:
-				if velocity.x + lr_input * GROUND_ACCELERATION * delta >= -GROUND_MAX_SPEED:
-					velocity.x += lr_input * GROUND_ACCELERATION * delta
+			elif velocity.x >= -GROUND_MAX_SPEED and lr_input_axis < -DEADBAND:
+				if velocity.x + lr_input_axis * GROUND_ACCELERATION * delta >= -GROUND_MAX_SPEED:
+					velocity.x += lr_input_axis * GROUND_ACCELERATION * delta
 				else:
 					velocity.x = -GROUND_MAX_SPEED
 			else:
-				if velocity.x > GROUND_ZERO_THRESHOLD:
+				if velocity.x > GROUND_ZERO_VELOCITY_THRESHOLD:
 					velocity.x -= GROUND_DECELERATION * delta
-				elif velocity.x < -GROUND_ZERO_THRESHOLD:
+				elif velocity.x < -GROUND_ZERO_VELOCITY_THRESHOLD:
 					velocity.x += GROUND_DECELERATION * delta
 				else:
 					velocity.x = 0
 		else:
-			if velocity.x <= AIR_MAX_SPEED and lr_input > DEADBAND:
-				if velocity.x + lr_input * AIR_ACCELERATION * delta <= AIR_MAX_SPEED:
-					velocity.x += lr_input * AIR_ACCELERATION * delta
+			if velocity.x <= AIR_MAX_SPEED and lr_input_axis > DEADBAND:
+				if velocity.x + lr_input_axis * AIR_ACCELERATION * delta <= AIR_MAX_SPEED:
+					velocity.x += lr_input_axis * AIR_ACCELERATION * delta
 				else:
 					velocity.x = AIR_MAX_SPEED
-			elif velocity.x >= -AIR_MAX_SPEED and lr_input < -DEADBAND:
-				if velocity.x + lr_input * AIR_ACCELERATION * delta >= -AIR_MAX_SPEED:
-					velocity.x += lr_input * AIR_ACCELERATION * delta
+			elif velocity.x >= -AIR_MAX_SPEED and lr_input_axis < -DEADBAND:
+				if velocity.x + lr_input_axis * AIR_ACCELERATION * delta >= -AIR_MAX_SPEED:
+					velocity.x += lr_input_axis * AIR_ACCELERATION * delta
 				else:
 					velocity.x = -AIR_MAX_SPEED
 			else:
-				if velocity.x > AIR_ZERO_THRESHOLD:
+				if velocity.x > AIR_ZERO_VELOCITY_THRESHOLD:
 					velocity.x -= AIR_DECELERATION * delta
-				elif velocity.x < -AIR_ZERO_THRESHOLD:
+				elif velocity.x < -AIR_ZERO_VELOCITY_THRESHOLD:
 					velocity.x += AIR_DECELERATION * delta
 				else:
 					velocity.x = 0
-
 		# Add jumping # NEED TO REPLACE IS_ON_FLOOR W/ AREA CHECK AT SOME POINT # NEED TO ADD VARIABLE HEIGHT JUMPING
-		# Ground jumping
-		if jump_input:
+		# NEED TO IMPLEMENT SUPERS/HYPERS? WHICH REQUIRES MODIFYING DASH (REWRITE TO NOT USE LERP AND TO ADD VELO INSTEAD OF SETTING)
+		if is_on_floor() or is_climbing or is_sliding:
+			ground_jumps = MAX_GROUND_JUMPS
+			air_jumps = MAX_AIR_JUMPS
+		
+		if Input.is_action_just_released("jump"):
+			is_jump_just_released = true
+		
+		if is_jump_input_pressed and ground_jumps > 0 and not is_jumping and (is_on_floor() or is_climbing or is_sliding):
 			if is_on_floor():
-				velocity.y = -GROUND_JUMP_SPEED
+				jump_start_location = "ground"
+				ground_jumps -= 1
 			elif is_climbing:
-				if lr_input == 0:
-					velocity.y = -WALL_CLIMBING_JUMP_VELOCITY.y
+				if lr_input_axis == 0:
+					jump_start_location = "climbing"
+				ground_jumps -= 1
 			elif is_sliding:
-				if lr_input == 0:
+				if lr_input_axis == 0:
+					jump_start_location = "sliding"
+				ground_jumps -= 1
+
+			jump_timer = 0.0
+			is_jumping = true
+		elif is_jump_input_pressed and air_jumps > 0 and not is_jumping and is_jump_just_released:
+			jump_start_location = "air"
+			air_jumps -= 1
+			
+			jump_timer = 0.0
+			is_jumping = true
+
+		if is_jumping and jump_timer <= JUMP_MAX_DURATION and is_jump_input_pressed:
+			jump_timer += delta
+			is_jump_just_released = false
+			if jump_start_location == "ground":
+				velocity.y = -GROUND_JUMP_SPEED
+			elif jump_start_location == "climbing":
+				if lr_input_axis == 0:
+					velocity.y = -WALL_CLIMBING_JUMP_VELOCITY.y
+			elif jump_start_location == "sliding":
+				if lr_input_axis == 0:
 					velocity.y = -WALL_SLIDING_JUMP_VELOCITY.y
+			elif jump_start_location == "air":
+				velocity.y = -AIR_JUMP_SPEED
+		else:
+			is_jumping = false
 
 	move_and_slide()
-	
-
 
 func _on_left_wall_detection_area_2d_body_entered(body: Node2D) -> void:
 	if body.get_name() != "Player2D":
